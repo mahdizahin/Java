eb0008ad18bd0445c73848f5894b0bd12157da43
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-06-21 22:52:27 +0600

    Add files via upload

---
+++ Vs.java
@@ -0,0 +1,11 @@
+public class Vs 
+{
+    public static void main(String[] args) {
+      System.out.println ("Bye world");
+    }
+  
+  
+    //I m learning java
+  }
+  
+  
---
+++ input.java
@@ -0,0 +1,18 @@
+ 
+  import java.util.Scanner;
+  class input{
+public static void main (string []args){
+    Scanner input = new Scanner (system.in);
+
+    System.out.print ("Enter your first name : ");
+    String firstname = input.next();
+
+    System.out.print ("Enter your last name : ");
+    String lastname = input.next();
+
+    System.out.println(" Your full name is "+firstname+" "+lastname);
+
+
+}
+  }  
+
