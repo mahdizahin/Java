4b585e82d07d4b206cf329797c72b54b10cb2763
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-07-27 22:06:12 +0600

    Add files via upload

---
+++ Recursion/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+	public static void main(String[] args) {
+		Recursion obj = new Recursion ();
+		int result = obj.fact(5);
+		System.out.println("Factorial of Five is : "+result);
+	}
+
+}
