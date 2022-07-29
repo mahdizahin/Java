f93af00d599cb07bdb8cd411532f0d2a423c4f6f
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-07-30 00:37:43 +0600

    Add files via upload

---
+++ Polymorphism/school/Main.java
@@ -0,0 +1,14 @@
+package school;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Person s1 = new Junior_Student();
+		 Person t1 = new Teacher();
+		 
+		 s1.show();
+		 t1.show();
+
+	}
+
+}
---
+++ Polymorphism/school/Teacher.java
@@ -0,0 +1,8 @@
+package school;
+
+public class Teacher extends Person{
+	public void show() {
+		System.out.println("Teacher Class");
+	}
+
+}
