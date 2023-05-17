3a6e113774df50c37139eb7f28a9e707bc3b039b
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-05-17 12:13:47 +0600

    Add files via upload

---
+++ If else/if_else.java
@@ -0,0 +1,33 @@
+//import java.util.Scanner;
+
+
+
+public class if_else
+{
+
+
+	public static void main(String args[])
+	{
+
+		System.out.println("What have you done boy!");
+
+		int n=7;
+
+		if(n>5)
+		{
+
+			for(int i=0;i<5;i++)
+			{
+				System.out.println(i);
+			}
+		}else
+		{
+			for(int i=7;i<17;i++)
+			{
+				System.out.println(i);
+			}
+		}
+
+		System.out.println("Now we are going in Eclipes")
+	}
+}
