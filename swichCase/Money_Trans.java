3d8f5cb7975e4506cedfbc67eaf186bf6a84e961
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-10-31 15:42:19 +0600

    Add files via upload

---
+++ swichCase/Money_Trans.java
@@ -0,0 +1,43 @@
+package swichCase;
+import java.util.Scanner;
+public class Money_Trans {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1. Money Transfer\n2. Send Money\n3. Payment\n4. Check Balance\n5. Home ");
+		int num = sc.nextInt();
+		
+		boolean r = true;
+		//while(r) {
+		
+		if (num<7) {
+		
+		switch(num){
+			case 1 :
+				System.out.println("Transection Complete");
+				break;
+			case 2 :
+				 
+				System.out.println("Enter Reciever Number");
+				
+				break;
+			case 3 :
+				System.out.println("Enter Marchant Number");
+				break;
+			case 4 :
+				System.out.println("Check Balance");
+				break;
+			case 5 :
+				System.out.println("Here is Home");
+				break;
+				
+		}
+
+	}else {
+		System.out.println("You typed Wrong");
+	}
+		//}
+		
+	}
+
+}
---
+++ swichCase/SwichCase.java
@@ -0,0 +1,32 @@
+package swichCase;
+
+import java.util.Scanner;
+
+public class SwichCase {
+
+	public static void main(String[] args) {
+	 Scanner input = new Scanner (System.in);
+	 // num;
+	 System.out.print("Enter number ");
+	 int num  = input.nextInt();
+	 switch(num) {
+	 
+	 case 1:
+		 System.out.println("You Typed one");
+		 break;
+	 case 2:
+		 System.out.println("You Typed Two");
+		 break;
+	 case 3:
+		 System.out.println("You Typed Three");
+		 break;
+	 case 4:
+		 System.out.println("You Typed Four");
+		 break;
+		 
+		 
+	 }
+	 
+	}
+
+}
